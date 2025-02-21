66e62085d489848bbed7e6aa31accde13672f839
shoyonp <shoyonkumar143@gmail.com>
2025-02-21 12:04:10 +0600

    inital footer created

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Home from "../pages/Home";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-           <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-190px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
